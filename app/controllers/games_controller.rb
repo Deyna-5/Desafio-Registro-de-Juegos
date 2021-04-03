2e94c9183c8ea40266f47b645e4cbed7e7bc6a80
Deyna Contreras <deyna.alexac@gmail.com>
2021-04-02 21:42:46 -0300

    Desafio culminado

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,64 @@
+class GamesController < ApplicationController
+  before_action :set_game, only: %i[ show edit update destroy ]
+
+  # GET /games or /games.json
+  def index
+    @games = Game.all
+  end
+
+  # GET /games/1 or /games/1.json
+  def show
+  end
+
+  # GET /games/new
+  def new
+    @game = Game.new
+  end
+
+  # GET /games/1/edit
+  def edit
+  end
+
+  # POST /games or /games.json
+  def create
+    @game = Game.new(game_params)
+
+    respond_to do |format|
+      if @game.save
+        format.html { redirect_to root_path, notice: "El juego se creó de forma éxitosa" }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /games/1 or /games/1.json
+  def update
+    respond_to do |format|
+      if @game.update(game_params)
+        format.html { redirect_to root_path, notice: "El juego de actualizó de forma éxitosa" }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /games/1 or /games/1.json
+  def destroy
+    @game.destroy
+    respond_to do |format|
+      format.html { redirect_to games_url, notice: "El juego se borró correctamente" }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_game
+      @game = Game.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def game_params
+      params.require(:game).permit(:name, :rule_text, :cover, :rule, pieces: [])
+    end
+end
